4c145f75dd66fc860a8640e0a0ee2370a6a03cc3
Marcello Faria <01faria.marcello@gmail.com>
2022-02-26 13:04:59 +0700

    fix convention

---
+++ App/Migrations/20220226055803_user_create.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace if3250_2022_19_filantropi_backend.Migrations
+{
+    public partial class user_create : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_users",
+                table: "users");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_galangan_dana",
+                table: "galangan_dana");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "pk_users",
+                table: "users",
+                column: "id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "pk_galangan_dana",
+                table: "galangan_dana",
+                column: "id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "pk_users",
+                table: "users");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "pk_galangan_dana",
+                table: "galangan_dana");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_users",
+                table: "users",
+                column: "id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_galangan_dana",
+                table: "galangan_dana",
+                column: "id");
+        }
+    }
+}
